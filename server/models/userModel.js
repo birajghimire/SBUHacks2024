98554e4b01f9001f30b88b95d50f8c430488d77c
birajghimire <47505992+birajghimire@users.noreply.github.com>
2024-02-10 02:04:22 -0500

    finished making my template for all of my routes

---
+++ server/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    lastLogin: Date
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

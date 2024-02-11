0fd30814a4a0a2477d96a7e7ade3f0ca14f7f687
skydaz123 <haowdy26@gmail.com>
2024-02-10 21:09:41 -0500

    committing for pulling

--- client/src/components/journalBox.js
+++ client/src/components/journalBox.js
@@ -11,7 +11,7 @@ const JournalBox = ({ backgroundColor, text, patternImage }) => {
       <div className="w-[15%] h-full absolute top-0 left-0 border-slate-400 border-r-2"></div>
       <div className="flex justify-center font-amatic-sc absolute top-8 left-4 right-0">
         <h2 className="text-xl h-1/5 font-semibold border-2 bg-white border-black rounded-lg text-gray-800 pt-3 pr-4 pl-4 box-border">
-          hello world
+          {text}
         </h2>
       </div>
     </div>

27b0d8409b28f8069fc50edf3ed37202b830e8ce
birajghimire <47505992+birajghimire@users.noreply.github.com>
2024-02-10 20:41:01 -0500

    adding axios changes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/navbar";
 import SearchBox from "../components/searchBox";
 import JournalBox from "../components/journalBox";
-import PlusButton from "../components/plusButton"; 
+import PlusButton from "../components/plusButton";
 import JournalModal from "../components/journalModal";
+import axios from "axios";
 
 export default function HomePage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [journals, setJournals] = useState([]);
+
+  const getJournals = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/journal");
+      setJournals(response.data);
+    } catch (error) {
+      console.log("There was an errr fetching the journals", error);
+    }
+  };
+
+  useEffect(() => {
+    getJournals();
+  }, []);
+
+  const handleJournalCreated = () => {
+    getJournals();
+  };
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -22,13 +41,13 @@ export default function HomePage() {
       <SearchBox />
       <div className="flex flex-wrap flex-row gap-6 h-screen pl-28">
         <JournalBox />
-        <JournalBox />
-        <JournalBox />
-        <JournalBox />
-        <JournalBox />
       </div>
       <PlusButton onClick={openModal} />
-      <JournalModal isOpen={isModalOpen} onClose={closeModal} />
+      <JournalModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        onJournalCreated={handleJournalCreated}
+      />
     </div>
   );
 }
